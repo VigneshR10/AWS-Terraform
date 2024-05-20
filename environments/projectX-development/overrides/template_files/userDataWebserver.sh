ffe48b8faaa5d339f392baf89ab17d4b6b815d26
Vignesh Ramalingam <30545541+VigneshR10@users.noreply.github.com>
2024-05-20 23:06:26 +0530

    Update userDataWebserver.sh
    Redaction commit

--- environments/projectX-development/overrides/template_files/userDataWebserver.sh
+++ environments/projectX-development/overrides/template_files/userDataWebserver.sh
@@ -7,11 +7,11 @@ mkdir rt-deployment
 chown -R ubuntu:ubuntu rt-deployment
 cd /home/ubuntu/rt-deployment
 ssh-add /home/ubuntu/.ssh/rt-messages-web
-git clone git@bitbucket.org:dev_teezle/dataviewer-webserver.git
-cd /home/ubuntu/rt-deployment/dataviewer-webserver
+git clone git@bitbucket.org:dev_xyz/sample_repo.git
+cd /home/ubuntu/rt-deployment/sample_repo
 npm install
-cd /home/ubuntu/rt-deployment/dataviewer-webserver/bin
-forever web-ui-server.js
+cd /home/ubuntu/rt-deployment/sample_repo/bin
+forever server.js
 echo "Deployment Process Done" " >> runDeployment
 chown ubuntu:ubuntu runDeployment
 /bin/bash /home/ubuntu/runDeployment
